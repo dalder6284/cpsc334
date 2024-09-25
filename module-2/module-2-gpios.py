961a0d4da21967b8acb6b6e444ca0caa9192ab68
dalder6284 <diegoalderetellaca@gmail.com>
2024-09-25 00:58:38 -0400

    Committing GPIO work

--- module-2/module-2-gpios.py
+++ module-2/module-2-gpios.py
@@ -1,26 +1,57 @@
 from gpiozero import Button
 from signal import pause
 import os
+import time
+
+os.system("clear")  
 
 button = Button(2)
 switch = Button(18)
 stick_y = Button(24)
 stick_x = Button(25)
 
-# Button changes mode of operation.
-STATE = 0
+switch_pressed = False
+state = 0
 
-def change_state():
-    STATE = (STATE + 1) % 3
-    os.system("clear")
+def handle_button():
+    global state
+    state = (state + 1) % 3
 
-button.when_pressed = change_state
+def switch_on():
+    global switch_pressed
+    global state
+    if state == 0:
+        print('A') 
+    elif state == 1:
+        print('B')
+    else:
+        print('C')
 
-#
-def switch_on()
-    # Print three dots in a sequence with half a second in between each dot, resetting to no dots when all three dots are printed.
+def stick_y_on():
+    global switch_pressed
+    global state
+    if state == 0:
+        print('.') 
+    elif state == 1:
+        print('!')
+    else:
+        print('?')
 
+def stick_x_on():
+    global switch_pressed
+    global state
+    if state == 0:
+        print('<') 
+    elif state == 1:
+        print('>')
+    else:
+        print('=')
+
+button.when_pressed = handle_button
+stick_y.when_pressed = stick_y_on
+stick_x.when_pressed = stick_x_on
 
 switch.when_pressed = switch_on
+
 
 pause()
